1afbfbc6d55cba0c04f04bf630ae6684e8819265
Zach Smith <x.coder.zach@gmail.com>
2012-06-11 16:12:26 -0500

    create operation diffs"

--- transform.js
+++ transform.js
@@ -42,7 +42,9 @@ function getSequenceFromXML(xml, callback) {
     , seq = []
 
   parser.ontext = function(text) {
-    [].push.apply(seq, text.split(''))
+    [].push.apply(seq, text.split('').map(function(character) {
+      return { type: "character", character: character }
+    }))
   }
 
   parser.onopentag = function(node) {
@@ -51,12 +53,12 @@ function getSequenceFromXML(xml, callback) {
       tag += " " + i + ' = "' + entity(tag.attributes[i]) + '"'
     }
     tag += ">"
-    seq.push(tag)
+    seq.push({type: "open", tag: tag, tagType: node.name })
   }
 
   parser.onclosetag = function(name) {
     var tag = "</" + name + ">"
-    seq.push(tag)
+    seq.push({type: "close", tag: tag, tagType: name })
   }
 
   parser.onend = function() {
@@ -66,18 +68,56 @@ function getSequenceFromXML(xml, callback) {
   parser.write(xml).close()
 }
 
-getSequenceFromXML("<xml>herp<stuff></stuff></xml>", function(seq) {
-  console.log(seq)
-  console.log(apply(seq
-      , [ {type: "deleteOpenTag", tag: "<xml>"}
-        , {type: "retain"}
-        , {type: "retain"}
-        , {type: "retain"}
-        , {type: "retain"}
-        , {type: "insertOpenTag", tag: "<herp>", tagType: "herp"}
-        , {type: "retain"}
-        , {type: "retain"}
-        , {type: "insertCloseTag", tag: "</herp>", tagType: "herp"}
-        , {type: "deleteCloseTag", tag: "</xml>" } ]))
+function compare(item1, item2) {
+  return item1.character && item1.character === item2.character 
+      || item1.tag && item1.tag === item2.tag 
+}
 
+/**
+ * Do this the lazy way, compute the common prefix and the common suffix,
+ * then delete everything in between and insert the new stuff
+ */
+function getDiffOperations(seq1, seq2) {
+  var prefix = 0
+    , suffix = 0
+    , diff = []
+    , i = 0
+
+  while(compare(seq1[prefix], seq2[prefix])) {
+    diff.push({type: "retain"})
+    prefix++
+  }
+  while(compare(seq1[seq1.length - suffix - 1], seq2[seq2.length - suffix - 1])) { 
+    suffix++
+  }
+  for(i = prefix ; i < seq1.length - suffix ; i++) {
+    if(seq1[i].type === "open") {
+      diff.push({ type: "deleteOpenTag", tag: seq1[i].tag, tagType: seq1[i].tagType })
+    } else if(seq1[i].type === "close") {
+      diff.push({ type: "deleteCloseTag", tag: seq1[i].tag, tagType: seq1[i].tagType })
+    } else if(seq1[i].type === "character") {
+      diff.push({ type: "delete", character: seq1[i].character })
+    } 
+  }
+  for(i = prefix ; i < seq2.length - suffix ; i++) {
+    if(seq2[i].type === "open") {
+      diff.push({ type: "insertOpenTag", tag: seq2[i].tag, tagType: seq2[i].tagType })
+    }
+    if(seq2[i].type === "close") {
+      diff.push({ type: "insertCloseTag", tag: seq2[i].tag, tagType: seq2[i].tagType })
+    }
+    if(seq2[i].type === "character") {
+      diff.push({ type: "insert", character: seq2[i].character })
+    }
+  }
+  for(i = 0 ; i < suffix ; i++) {
+    diff.push({type: "retain"})
+  }
+  return diff
+}
+
+getSequenceFromXML("<xml>herpsdfa</xml>", function(seq1) {
+  getSequenceFromXML("<xml>herp<stuff></stuff></xml>", function(seq2) {
+    console.log(getDiffOperations(seq1, seq2))
+  })
 })
